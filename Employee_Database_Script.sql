2650033e85fcc12566db725141641539d9d1ecab
harjot85 <harjot85@users.noreply.github.com>
2017-03-31 13:40:55 -0700

    Update Employee_Database_Script.sql
    I have modified the code according to the style and structure of the attribute names and table names that matches the style in the Fundamentals of Database Systems 6th Edition.

--- Employee_Database_Script.sql
+++ Employee_Database_Script.sql
@@ -1,115 +1,118 @@
-CREATE TABLE Department (
-  d_name         char(20)    NOT NULL,
-  d_number       numeric(1)  NOT NULL,  
-  mgr_ssn        numeric(9)  NOT NULL,   
-  mgr_startdate  date        NOT NULL, 
-  PRIMARY KEY   (d_number), 
-  UNIQUE        (d_name)
-);
+CREATE TABLE EMPLOYEE 
+( Fname           VARCHARchar(10)   NOT NULL, 
+  Minit           CHAR,
+  Lname           VARCHAR(20),      NOT NULL, 
+  Ssn             CHAR(9),          NOT NULL,   
+  Bdate           DATE,        
+  Address         VARCHAR(30),    
+  Sex             CHAR(1),     
+  Salary          DECIMAL(5),  
+  Super_ssn       CHAR(9),
+  Dno             INT               NOT NULL, 
+PRMARY KEY   (Ssn),
+FOREIGN KEY  (Super_ssn) REFERENCES EMPLOYEE(Ssn),
+FOREIGN KEY  (Dno) REFERENCES Department(d_number) ); 
 
-CREATE TABLE Employee (
-  f_name    char(10)    NOT NULL, 
-  l_name    char(20)    NOT NULL, 
-  ssn       numeric(9)  NOT NULL,   
-  bdate     date        NOT NULL, 
-  address   char(30)    NOT NULL, 
-  sex       char(1)     NOT NULL,
-  salary    numeric(5)  NOT NULL, 
-  super_ssn numeric(9),
-  d_no      numeric(1)  NOT NULL, 
-  PRMARY KEY  (ssn), 
-  FOREIGN KEY (d_no) REFERENCES Department(d_number)
-); 
+CREATE TABLE DEPARTMENT
+( Dname           VARCHAR(15)       NOT NULL,
+  Dnumber         INT               NOT NULL,
+  Mgr_ssn         CHAR(9)           NOT NULL,
+  Mgr_start_date  DATE,
+PRIMARY KEY (Dnumber),
+UNIQUE      (Dname),
+FOREIGN KEY (Mgr_ssn) REFERENCES EMPLOYEE(Ssn) );
 
-CREATE TABLE Project (
-  p_name      char(20)    NOT NULL, 
-  p_number    numeric(2)  NOT NULL, 
-  p_location  char(20)    NOT NULL, 
-  d_num       numeric(1)  NOT NULL, 
-  PRIMARY KEY (p_number), 
-  UNIQUE      (p_name), 
-  FOREIGN KEY (d_num) REFERENCES Department(d_number)
-); 
+CREATE TABLE DEPT_LOCATIONS
+( Dnumber         INT               NOT NULL,
+  Dlocation       VARCHAR(15)       NOT NULL,
+PRIMARY KEY (Dnumber, Dlocation),
+FOREIGN KEY (Dnumber) REFERENCES DEPARTMENT(Dnumber) );
 
-CREATE TABLE Works_on (
-  essn        numeric(9) NOT NULL, 
-  p_no        numeric(2) NOT NULL,  
-  hours       numeric(5,1),  
-  PRIMARY KEY (essn, p_no), 
-  FOREIGN KEY (essn) REFERENCES Employee(ssn),
-  FOREIGN KEY (p_no) REFERENCES Project(p_number)
-);
+CREATE TABLE PROJECT
+( Pname           VARCHAR(15)       NOT NULL,
+  Pnumber         INT               NOT NULL,
+  Plocation       VARCHAR(15),
+  Dnum            INT               NOT NULL,
+PRIMARY KEY (Pnumber),
+UNIQUE      (Pname),
+FOREIGN KEY (Dnum) REFERENCES DEPARTMENT(Dnumber) ); 
 
-CREATE TABLE Dependent (
-  essn            numeric(9)  NOT NULL,   
-  dependent_name  char(10)    NOT NULL, 
-  sex             char(1)     NOT NULL, 
-  bdate           date        NOT NULL, 
-  relationship    char(30)    NOT NULL, 
-  PRIMARY KEY     (essn, dependent_name),
-  FOREIGN KEY     (essn) REFERENCES Employee(ssn)
-); 
+CREATE TABLE WORKS_ON
+( Essn            CHAR(9)           NOT NULL,
+  Pno             INT               NOT NULL,
+  Hours           DECIMAL(3,1)      NOT NULL,
+PRIMARY KEY (Essn, Pno),
+FOREIGN KEY (Essn) REFERENCES EMPLOYEE(Ssn),
+FOREIGN KEY (Pno) REFERENCES PROJECT(Pnumber) );
 
-create table Dept_locations (
-  d_number    numeric(1)  NOT NULL, 
-  d_location  char (15)   NOT NULL, 
-  PRIMARY KEY (d_number, d_location), 
-  FOREIGN KEY (d_number) REFERENCES Department(d_number)
-);
+CREATE TABLE DEPENDENT
+( Essn            CHAR(9)           NOT NULL,
+  Dependent_name  VARCHAR(15)       NOT NULL,
+  Sex             CHAR,
+  Bdate           DATE,
+  Relationship    VARCHAR(8),
+PRIMARY KEY (Essn, Dependent_name),
+FOREIGN KEY (Essn) REFERENCES EMPLOYEE(Ssn) );
 
-INSERT INTO Department VALUES   ('Research',5,333445555,'1988-05-22'),
-                                ('Administration',4,987654321,'1995-01-01'),
-                                ('Headquarters',1,888665555,'1981-06-19');
+INSERT INTO DEPARTMENT 
+VALUES      ('Research',5,333445555,'1988-05-22'),
+            ('Administration',4,987654321,'1995-01-01'),
+            ('Headquarters',1,888665555,'1981-06-19');
 
-INSERT INTO Employee VALUES     ('John','Smith',123456789,'1965-01-09','731 Fondren, Houston TX','M',30000,333445555,5),
-                                ('Franklin','Wong',333445555,'1965-12-08','638 Voss, Houston TX','M',40000,888665555,5),
-                                ('Alicia','Zelaya',999887777,'1968-01-19','3321 Castle, Spring TX','F',25000,987654321,4),
-                                ('Jennifer','Wallace',987654321,'1941-06-20','291 Berry, Bellaire TX','F',43000,888665555,4),
-                                ('Ramesh','Narayan',666884444,'1962-09-15','975 Fire Oak, Humble TX','M',38000,333445555,5),
-                                ('Joyce','English',453453453,'1972-07-31','5631 Rice, Houston TX','F',25000,333445555,5),
-                                ('Ahmad','Jabbar',987987987,'1969-03-29','980 Dallas, Houston TX','M',25000,987654321,4),
-                                ('James','Borg',888665555,'1937-11-10','450 Stone, Houston TX','M',55000,null,1);
+INSERT INTO EMPLOYEE 
+VALUES      ('John','Smith',123456789,'1965-01-09','731 Fondren, Houston TX','M',30000,333445555,5),
+            ('Franklin','Wong',333445555,'1965-12-08','638 Voss, Houston TX','M',40000,888665555,5),
+            ('Alicia','Zelaya',999887777,'1968-01-19','3321 Castle, Spring TX','F',25000,987654321,4),
+            ('Jennifer','Wallace',987654321,'1941-06-20','291 Berry, Bellaire TX','F',43000,888665555,4),
+            ('Ramesh','Narayan',666884444,'1962-09-15','975 Fire Oak, Humble TX','M',38000,333445555,5),
+            ('Joyce','English',453453453,'1972-07-31','5631 Rice, Houston TX','F',25000,333445555,5),
+            ('Ahmad','Jabbar',987987987,'1969-03-29','980 Dallas, Houston TX','M',25000,987654321,4),
+            ('James','Borg',888665555,'1937-11-10','450 Stone, Houston TX','M',55000,null,1);
 
-INSERT INTO Project VALUES      ('ProductX',1,'Bellaire',5),
-                                ('ProductY',2,'Sugarland',5),
-                                ('ProductZ',3,'Houston',5),
-                                ('Computerization',10,'Stafford',4),
-                                ('Reorganization',20,'Houston',1),
-                                ('Newbenefits',30,'Stafford',4);
+INSERT INTO PROJECT 
+VALUES      ('ProductX',1,'Bellaire',5),
+            ('ProductY',2,'Sugarland',5),
+            ('ProductZ',3,'Houston',5),
+            ('Computerization',10,'Stafford',4),
+            ('Reorganization',20,'Houston',1),
+            ('Newbenefits',30,'Stafford',4);
 
-INSERT INTO Works_on VALUES     (123456789,1,32.5),
-                                (123456789,2,7.5),
-                                (666884444,3,40.0),
-                                (453453453,1,20.0),
-                                (453453453,2,20.0),
-                                (333445555,2,10.0),
-                                (333445555,3,10.0),
-                                (333445555,10,10.0),
-                                (333445555,20,10.0),
-                                (999887777,30,30.0),
-                                (999887777,10,10.0),
-                                (987987987,10,35.0),
-                                (987987987,30,5.0),
-                                (987654321,30,20.0),
-                                (987654321,20,15.0),
-                                (888665555,20,null);
+INSERT INTO WORKS_ON 
+VALUES     (123456789,1,32.5),
+           (123456789,2,7.5),
+           (666884444,3,40.0),
+           (453453453,1,20.0),
+           (453453453,2,20.0),
+           (333445555,2,10.0),
+           (333445555,3,10.0),
+           (333445555,10,10.0),
+           (333445555,20,10.0),
+           (999887777,30,30.0),
+           (999887777,10,10.0),
+           (987987987,10,35.0),
+           (987987987,30,5.0),
+           (987654321,30,20.0),
+           (987654321,20,15.0),
+           (888665555,20,null);
 
-INSERT INTO Dependent VALUES    (333445555,'Alice','F','1986-04-04','Daughter'),
-                                (333445555,'Theodore','M','1983-10-25','Son'),
-                                (333445555,'Joy','F','1958-05-03','Spouse'),
-                                (987654321,'Abner','M','1942-02-28','Spouse'),
-                                (123456789,'Michael','M','1988-01-04','Son'),
-                                (123456789,'Alice','F','1988-12-30','Daughter'),
-                                (123456789,'Elizabeth','F','1967-05-05','Spouse');
+INSERT INTO DEPENDENT 
+VALUES      (333445555,'Alice','F','1986-04-04','Daughter'),
+            (333445555,'Theodore','M','1983-10-25','Son'),
+            (333445555,'Joy','F','1958-05-03','Spouse'),
+            (987654321,'Abner','M','1942-02-28','Spouse'),
+            (123456789,'Michael','M','1988-01-04','Son'),
+            (123456789,'Alice','F','1988-12-30','Daughter'),
+            (123456789,'Elizabeth','F','1967-05-05','Spouse');
 
-INSERT INTO dept_locations VALUES (1,'Houston'),
-                                  (4,'Stafford'),
-                                  (5,'Bellaire'),
-                                  (5,'Sugarland'), 
-                                  (5,'Houston');
+INSERT INTO DEPT_LOCATIONS 
+VALUES      (1,'Houston'),
+            (4,'Stafford'),
+            (5,'Bellaire'),
+            (5,'Sugarland'), 
+            (5,'Houston');
 
-ALTER TABLE Department 
- ADD Constraint Dep_emp FOREIGN KEY (mgr_ssn) REFERENCES Employee(ssn);
+ALTER TABLE DEPARTMENT 
+ ADD CONSTRAINT Dep_emp FOREIGN KEY (Mgr_ssn) REFERENCES EMPLOYEE(Ssn);
 
-ALTER TABLE Employee   
- ADD Constraint Emp_emp FOREIGN KEY (super_ssn) REFERENCES Employee(ssn);
+ALTER TABLE EMPLOYEE   
+ ADD CONSTRAINT Emp_emp FOREIGN KEY (Super_ssn) REFERENCES EMPLOYEE(Ssn);
