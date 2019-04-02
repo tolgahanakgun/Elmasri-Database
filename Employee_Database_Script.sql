89ab5e1c0ee7b823e446a62c03fb6466a914a434
Charlie Russ <cruss@gatech.edu>
2019-04-01 19:25:22 -0700

    Updates for removing foreign key issues.

--- Employee_Database_Script.sql
+++ Employee_Database_Script.sql
@@ -1,17 +1,15 @@
 CREATE TABLE EMPLOYEE
 ( Fname           VARCHAR(10)   NOT NULL,
   Minit           CHAR,
-  Lname           VARCHAR(20),      NOT NULL,
-  Ssn             CHAR(9),          NOT NULL,
+  Lname           VARCHAR(20)      NOT NULL,
+  Ssn             CHAR(9)          NOT NULL,
   Bdate           DATE,
   Address         VARCHAR(30),
   Sex             CHAR(1),
   Salary          DECIMAL(5),
   Super_ssn       CHAR(9),
   Dno             INT               NOT NULL,
-PRIMARY KEY   (Ssn),
-FOREIGN KEY  (Super_ssn) REFERENCES EMPLOYEE(Ssn),
-FOREIGN KEY  (Dno) REFERENCES DEPARTMENT(d_number) );
+PRIMARY KEY   (Ssn));
 
 CREATE TABLE DEPARTMENT
 ( Dname           VARCHAR(15)       NOT NULL,
@@ -54,11 +52,6 @@ CREATE TABLE DEPENDENT
 PRIMARY KEY (Essn, Dependent_name),
 FOREIGN KEY (Essn) REFERENCES EMPLOYEE(Ssn) );
 
-INSERT INTO DEPARTMENT
-VALUES      ('Research',5,333445555,'1988-05-22'),
-            ('Administration',4,987654321,'1995-01-01'),
-            ('Headquarters',1,888665555,'1981-06-19');
-
 INSERT INTO EMPLOYEE
 VALUES      ('John','B','Smith',123456789,'1965-01-09','731 Fondren, Houston TX','M',30000,333445555,5),
             ('Franklin','T','Wong',333445555,'1965-12-08','638 Voss, Houston TX','M',40000,888665555,5),
@@ -68,6 +61,11 @@ VALUES      ('John','B','Smith',123456789,'1965-01-09','731 Fondren, Houston TX'
             ('Joyce','A','English',453453453,'1972-07-31','5631 Rice, Houston TX','F',25000,333445555,5),
             ('Ahmad','V','Jabbar',987987987,'1969-03-29','980 Dallas, Houston TX','M',25000,987654321,4),
             ('James','E','Borg',888665555,'1937-11-10','450 Stone, Houston TX','M',55000,null,1);
+
+INSERT INTO DEPARTMENT
+VALUES      ('Research',5,333445555,'1988-05-22'),
+            ('Administration',4,987654321,'1995-01-01'),
+            ('Headquarters',1,888665555,'1981-06-19');
 
 INSERT INTO PROJECT
 VALUES      ('ProductX',1,'Bellaire',5),
@@ -93,7 +91,7 @@ VALUES     (123456789,1,32.5),
            (987987987,30,5.0),
            (987654321,30,20.0),
            (987654321,20,15.0),
-           (888665555,20,null);
+           (888665555,20,16.0);
 
 INSERT INTO DEPENDENT
 VALUES      (333445555,'Alice','F','1986-04-04','Daughter'),
@@ -116,3 +114,7 @@ ALTER TABLE DEPARTMENT
 
 ALTER TABLE EMPLOYEE
  ADD CONSTRAINT Emp_emp FOREIGN KEY (Super_ssn) REFERENCES EMPLOYEE(Ssn);
+ALTER TABLE EMPLOYEE
+ ADD CONSTRAINT Emp_dno FOREIGN KEY  (Dno) REFERENCES DEPARTMENT(Dnumber);
+ALTER TABLE EMPLOYEE
+ ADD CONSTRAINT Emp_super FOREIGN KEY  (Super_ssn) REFERENCES EMPLOYEE(Ssn);
